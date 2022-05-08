32882b260c2ca49c3024e4b17fea7b2812a9d089
Sam Miller <sam.dakota@gmail.com>
2022-05-08 17:01:33 -0400

    fixed html

--- index.js
+++ index.js
@@ -2,23 +2,156 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateHTML = require('./src/generateHTML');
 
-// function to generate HTML file
-function createHTML(fileName, data) {
-    fs.writeFile(fileName, generateHTML(data), err =>
-    err ? console.log(err) : console.log('HTML generated'))
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+let teamArray = [];
+let employeeIdArray = [];
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the manager's name?", 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the manager's employee id?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's office number?",
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the Engineer's name?", 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the engineer's employee id?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the engineer's email address?",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's github user name?",
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the intern's name?", 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the intern's id number?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the intern's email address?",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What is the intern's school?",
+    }
+];
+
+// function to create team
+function addEmployee() {
+    inquirer
+        .prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Who would you like to add to the team?",
+            choices: ['Manager', 'Engineer', 'Intern', 'Team complete'] 
+        }
+    ])
+    .then((answers) => {
+        switch(answers.role) {
+            case 'Manager':
+                addManager()
+                break;
+            case 'Engineer': 
+                addEngineer()
+                break;
+            case 'Intern': 
+                addIntern()
+                break;
+            default: 
+                createTeam()
+            };
+        }
+    );
 };
 
-// function to initialize app
-function init() {
+function addManager() {
     inquirer
-        .prompt()
-        .then((answers) => {
-            createHTML('index.html', answers)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+    .prompt(managerQuestions)
+    .then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamArray.push(manager);
+        employeeIdArray.push(answers.id);
+        addEmployee();
+    })
+    .catch(err => {
+        console.log(err)
+    })
 };
+
+function addEngineer() {
+    inquirer
+    .prompt(engineerQuestions)
+    .then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamArray.push(engineer);
+        employeeIdArray.push(answers.id);
+        addEmployee();
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+function addIntern() {
+    inquirer
+    .prompt(internQuestions)
+    .then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamArray.push(intern);
+        employeeIdArray.push(answers.id);
+        addEmployee();
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+function createTeam() {
+    fs.writeFileSync('./dist/index.html', generateHTML(teamArray))
+}
 
 // call to initialize app
-init()
+addEmployee()
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,92 @@
-function generateHTML(data) {
-    return``;
+function generateTeamHTML(team) {
+
+    function generateManagerCard(data) {
+        return`
+        <div class="card d-flex m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Manager</h5>
+                <p class="card-text">Name: ${data.name}</p>
+                <p class="card-text">ID: ${data.id}</p>
+                <p class="card-text">Office Number: ${data.officeNumber}</p>
+                <p class="card-text">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+            </div>
+        </div>
+        `;
+    }
+
+    function generateInternCard(data) {
+        return `
+        <div class="card d-flex m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Intern</h5>
+                <p class="card-text">Name: ${data.name}</p>
+                <p class="card-text">ID: ${data.id}</p>
+                <p class="card-text">School: ${data.school}</p>
+                <p class="card-text">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+            </div>
+        </div>
+        `;
+    }
+
+    function generateEngineerCard(data) {
+        return`
+        <div class="card d-flex m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Engineer</h5>
+                <p class="card-text">Name: ${data.name}</p>
+                <p class="card-text">ID: ${data.id}</p>
+                <p class="card-text">Github: <a href="www.github.com/${data.github}">www.github.com/${data.github}</a></p>
+                <p class="card-text">Email: <a href="mailto:${data.email}">${data.email}</a></p>
+            </div>
+        </div>
+        `;
+    }
+
+    const HTMLArray = [];
+    HTMLArray.push(team
+             .filter(employee => employee.getRole() === 'Manager')
+             .map(manager => generateManagerCard(manager))
+             );
+    HTMLArray.push(team
+            .filter(employee => employee.getRole() === 'Engineer')
+            .map(engineer => generateEngineerCard(engineer))
+            .join('')
+            );
+    HTMLArray.push(team
+            .filter(employee => employee.getRole() === 'Intern')
+            .map(intern => generateInternCard(intern))
+            .join('')
+            );
+    return HTMLArray.join('');
 }
 
-module.exports = generateHTML;
+module.exports = finalTeam => {
+    return `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Team</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        </head>
+        <body>
+            <header>
+                <nav class="navbar bg-light mb-5" id="navbar">
+                    <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
+                </nav>
+            </header>
+            <main>
+                <div class="container d-flex flex-wrap">
+                    <div class="d-flex flex-wrap justify-content-center" id="team-cards">
+                        ${generateTeamHTML(finalTeam)}
+                    </div>
+                </div>
+            </main>
+            
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+    </html>
+    `
+};
